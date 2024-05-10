8ec1a6e8979a960f1beeabb35607473d3011aaf1
antden99 <antoniodenigris@icloud.com>
2024-05-10 17:26:42 +0200

    add layout home page

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -1,11 +1,14 @@
 <?php
-
 namespace App\Http\Controllers\Guests;
-
+use App\Models\Train;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+
+        $trainsList = Train::all();
+        return view('home',compact('trainsList'));
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,42 @@
 @extends('layout.app')
 
-
 @section('content')
+    <main class="bg-dark text-white">
+        <div class="container">
+            <h1 class="text-center">Trains Available</h1>
+            <div class="row">
+                @foreach ($trainsList as $train)
+                    <div class="col-md-4 g-4">
+                        <div class="card mb-4" style="height: 100%;">
+                            <div class="card-header">
+                                <h5>Dettagli Treno</h5>
+                            </div>
+                            <div class="card-body">
+                                @if ($train->agency)
+                                    <div class="mb-0"><strong>Agenzia:</strong> {{ $train->agency }}</div>
+                                @endif
 
-<main>
-    <h1>Sito Anto</h1>
-</main>
+                                @if ($train->departure_station)
+                                    <div class="mb-0"><strong>Stazione di Partenza:</strong>
+                                        {{ $train->departure_station }}</div>
+                                @endif
 
+                                @if ($train->destination)
+                                    <div class="mb-0"><strong>Destinazione:</strong> {{ $train->destination }}</div>
+                                @endif
+
+                                @if ($train->departure_time)
+                                    <div class="mb-0"><strong>Ora di Partenza:</strong> {{ $train->departure_time }}</div>
+                                @endif
+
+                                @if ($train->arrival_time)
+                                    <div class="mb-0"><strong>Ora di Arrivo:</strong> {{ $train->arrival_time }}</div>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
 @endsection
